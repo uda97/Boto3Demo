3bee482bd7c74af625f338e3343559a0f7700bd3
whr <insky731@gmail.com>
2023-11-27 02:13:00 +0900

    Add function 3, 5, 7, 99

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 
 ec2 = boto3.client('ec2')
 
@@ -9,15 +10,18 @@ def switch(select_code):
     elif select_code == 2:
         available_zones()
     elif select_code == 3:
-        return 3
+        input_instance_id = input('Enter instance id: ')
+        start_instance(input_instance_id)
     elif select_code == 4:
         available_regions()
     elif select_code == 5:
-        return 5
+        input_instance_id = input('Enter instance id: ')
+        stop_instance(input_instance_id)
     elif select_code == 6:
         return 6
     elif select_code == 7:
-        return 7
+        input_instance_id = input('Enter instance id: ')
+        reboot_instance(input_instance_id)
     elif select_code == 8:
         return 8
     elif select_code == 99:
@@ -57,6 +61,28 @@ def available_zones():
         print(f'[region]{s1:>15}, [Endpoint] {s2}')
 
 
+def start_instance(instance_id):
+    print(f'Starting ... {instance_id}')
+
+    # verify permissions
+    try:
+        ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # run without dryrun
+    try:
+        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+        server_status = response.get('StartingInstances')[0].get('PreviousState').get('Name')
+        if server_status == 'stopped':
+            print(f'Successfully started instance{instance_id}')
+        if server_status == 'running':
+            print(f'failure started instance: {instance_id} is already running')
+    except ClientError as e:
+        print(e)
+
+
 def available_regions():
     # Retrieves availability zones only for region of the ec2 object
     response = ec2.describe_availability_zones()
@@ -66,6 +92,55 @@ def available_regions():
         s2 = each_response.get('RegionName')
         s3 = each_response.get('ZoneName')
         print(f'[id] {s1:10}, [region] {s2:10}, [zone] {s3:10}')
+
+
+def stop_instance(instance_id):
+    print(f'Stopping ... {instance_id}')
+
+    # verify permissions
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # stop without dryrun
+    try:
+        response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
+        server_status = response.get('StoppingInstances')[0].get('PreviousState').get('Name')
+        if server_status == 'running':
+            print(f'Successfully stopped instance{instance_id}')
+        if server_status == 'stopped':
+            print(f'failure stopped instance: {instance_id} is already stopped')
+    except ClientError as e:
+        print(e)
+
+
+def create_instance():
+    # TODO
+    pass
+
+
+def reboot_instance(instance_id):
+    print(f'Rebooting ... {instance_id}')
+
+    try:
+        ec2.reboot_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            print("You don't have permission to reboot instances.")
+            raise
+
+    try:
+        response = ec2.reboot_instances(InstanceIds=[instance_id], DryRun=False)
+        print(f'Successfully stopped instance{instance_id}')
+    except ClientError as e:
+        print('Error', e)
+
+
+def list_images():
+    # TODO
+    pass
 
 
 def main():
@@ -81,9 +156,8 @@ def main():
               7. reboot instance              8. list images         
                                               99. quit          
               ------------------------------------------------------------            ''')
-        print("Enter an integer: ")
 
-        select_code = input()
+        select_code = input("Enter an integer: ")
         try:
             select_code = int(select_code)
             switch(select_code)
